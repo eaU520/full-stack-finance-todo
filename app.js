2370df831ac857b8433f812b22f17820e32f1e70
eaU520 <ecallowayllc@gmail.com>
2023-07-17 22:46:46 -0400

    Frontend issues

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 const express = require('express');
-const connectDB = require('./conn.mjs');
+// const connectDB = require('./server/db/conn.mjs');
 
 const app = express();
 
 var cors = require('cors');
 
 //routes
-const expensesAPI = require("/server/routes/expense");
+const expensesAPI = require("./server/routes/expense.mjs");
 
-connectDB();
+// connectDB();
 
 //cors
-app.use(cors({ origin: true, credentials: true}));
+// app.use(cors({ origin: true, credentials: true}));
 
-//JSON parsing/middleware
-// app.use(express.json({extended: false}));
+// //JSON parsing/middleware
+// // app.use(express.json({extended: false}));
 app.use(express.json());
 
 app.get('/', (req, res) => res.send(''));
 
 app.use("/expenses", expensesAPI);
 
-// app.use("/",()=>{res.render()});
+// // app.use("/",()=>{res.render()});
 
-app.use("/create-expense", expensesAPI);
+// app.use("/create-expense", expensesAPI);
 
-const port = process.env.PORT || 5050;
+// const port = process.env.PORT || 5050;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
-//TODO: DELETE
+// app.listen(port, () => console.log(`Server running on port ${port}`));
+// //TODO: DELETE
