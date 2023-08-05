518f28b629824330d82b541a20c44926a6f9c990
eaU520 <ecallowayllc@gmail.com>
2023-08-04 20:46:16 -0400

    Navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,13 @@
 import React from "react";
 
 import {Route, Routes} from "react-router-dom";
-// import Navbar from "./components/navbar";
 import ExpenseList from "./components/ShowExpenseList.js";
 import Create from "./components/CreateExpense.js";
-// TODO: THIs file does nothing , remove?
 const App = () => {
  return (
    <div>
      <Routes>
+      {/* TODO: Navigation and feel, Bootstrap */}
       <Route path="/" element={<ExpenseList />}  exact/>
        {/* <Route path='/view_expenses' element={<ExpenseList />} excat/> */}
        <Route path='/create_expense' element={<Create />} exact/>
