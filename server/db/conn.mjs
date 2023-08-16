58864a3302d4547a6d6a46aad53fd30a3acd4644
eaU520 <ecallowayllc@gmail.com>
2023-08-16 13:36:02 -0400

    Connection issues

--- server/db/conn.mjs
+++ server/db/conn.mjs
@@ -5,13 +5,13 @@ dotenv.config();
 
 const connectionString = process.env.ATLAS_URI || "";
 const client = new MongoClient(connectionString);
-
 let conn;
 try{
   conn = await client.connect();
 }catch(e){
   console.error("Could not connect: ",e);
 }
+console.log(`Connection: ${conn}`);
 let db = conn.db("test");
 export default db;
 
