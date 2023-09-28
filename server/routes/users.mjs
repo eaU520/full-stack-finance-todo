398cd72c3ffa6e8ed7c7d786b0d00976336fb5c3
eaU520 <ecallowayllc@gmail.com>
2023-09-28 07:50:19 -0400

    Checks username/name to register

--- server/routes/users.mjs
+++ server/routes/users.mjs
@@ -46,11 +46,9 @@ userRouter.post("/register",
   const collection = await db.collection("users");
   const existsEmail = await collection.find({email:  userAdd.email}).toArray();
   const existsUsername = await collection.find({username: userAdd.username}).toArray();
-  console.log("Email: ",existsEmail.length === 0);
-  console.log("Username: ", existsUsername.length === 0);
-  if (existsEmail._eventsCount === 0  && existsUsername._eventsCount === 0){//TODO: Hash password
+  console.log(userAdd, "Check inserting data");
+  if (existsEmail.length === 0  && existsUsername.length === 0){//TODO: Hash password
     const result = await collection.insertOne(userAdd);
-    // console.log("Added person", result, userAdd);
     response.send(result).status(204);
   }
   else{
