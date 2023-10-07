10c6eb2836f5fb3ea4fb1780e7d62cf8931f31e7
eaU520 <ecallowayllc@gmail.com>
2023-10-07 10:37:05 -0400

    Found redirect, navigate library

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route, Router, Routes} from "react-router-dom";
+import {Route, Routes, Navigate} from "react-router-dom";
 import ExpenseList from "./components/ShowExpenseList.js";
 import LoginUser from "./components/LoginUser.js";
 import RegisterUser from "./components/RegisterUser.js";
@@ -7,27 +7,27 @@ import Create from "./components/CreateExpense.js";
 import ForgotPassword from "./components/ForgotPassword.js";
 import Navigation from './components/Navigation.js';
 //TODO: Dark, contrast, light themes
-const requireAuth = (nextState, replace) =>{
+const requireAuth = () =>{
   console.log(this.authenticated, this);
   if(!this.authenticated()){
-    replace("/user/login");
+    return <Navigate to="/user/login" />;
+  }else{
+    return;
   }
 }
 
 const App = () => {
  return (
    <div>
-     <Router>
       <Routes>
       <Navigation/>
       {/* TODO: Feel, Bootstrap */}
       <Route path="/user/login" element={<LoginUser />}/>
-      <Route path='/' element={<ExpenseList />} onEnter={this.requireAuth}/>
-      <Route path='/expense/create_expense' element={<Create />} onEnter={this.requireAuth}/>
+      <Route path='/' element={<ExpenseList />} render={this.requireAuth()}/>
+      <Route path='/expense/create_expense' element={<Create />} render={this.requireAuth()}/>
       <Route path='/user/register' element={<RegisterUser />} />
       <Route path='/user/forgot' element={<ForgotPassword />} />
       </Routes>
-     </Router>
    </div>
  );
 };
