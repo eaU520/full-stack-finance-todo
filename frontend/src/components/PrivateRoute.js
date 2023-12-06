8c50ee186b79cceb604f28422b64711877ab6da2
eaU520 <ecallowayllc@gmail.com>
2023-12-06 05:31:18 -0500

    Removed extra logs for authentication

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -3,12 +3,6 @@ import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({Component}) =>{
     const authenticated = sessionStorage.getItem("session");
-    // const tempCook = response.clearCookie("session-id");
-   // req.session.user = null;
-   // response.cookie = "session=false";
-//    const tempSession =  window.sessionStorage.getItem("session-id");
-    console.log(`Authenticated ${authenticated}`);
-    //TODO: session is stored in req.session.user
     
     return authenticated ? <Component/> : <Navigate to="/users/login" />;
 }
