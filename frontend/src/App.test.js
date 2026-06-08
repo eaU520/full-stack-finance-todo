576a0a5bfe8d24dfbde54b676f8f400e28113dcd
eaU520 <ecallowayllc@gmail.com>
2026-06-08 18:31:52 -0400

    One passing

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,12 +1,13 @@
 /**
  * @jest-environment jsdom
  */
- import '@testing-library/jest-dom'
-import React from "react";
+import '@testing-library/jest-dom'
 import { render, screen, cleanup } from '@testing-library/react';
 import App from './App';
 
-afterEach(cleanup);
+afterEach(() => {
+  cleanup()
+});
 
 test('renders App', () => {
   // render(<App />);Issue with navigate and list of expenses
