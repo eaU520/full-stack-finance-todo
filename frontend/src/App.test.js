459545e5db9c853fa675c6d1b925162e8478d62b
eaU520 <ecallowayllc@gmail.com>
2026-06-10 06:02:34 -0400

    one test passing

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -4,13 +4,14 @@
 import '@testing-library/jest-dom'
 import { render, screen, cleanup } from '@testing-library/react';
 import App from './App';
+import React from "react";
 
 afterEach(() => {
   cleanup()
 });
 
 test('renders App', () => {
-  // render(<App />);Issue with navigate and list of expenses
+  render(<App />);//Issue with navigate and list of expenses
   // .toBeInTheDocument();
 
 });
